4bfdf3bf0b47f0da1a7caf9a45b2663b1ca24219
Sheyh <965598930@qq.com>
2022-07-08 19:42:38 +0800

    feat: 构建基本结构

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.urlencoded({extended:false}));
 
+//全局中间件
+app.use((req, res, next) => {
+    res.cc = function(err , status = 1){
+        
+    }
+    next()
+})
 
 const userRouter = require('./router/user');
 app.use('/user',userRouter);
--- router_handler/user.js
+++ router_handler/user.js
@@ -1,7 +1,14 @@
+const bcryptjs = require('bcryptjs')
+console.log(bcryptjs.hashSync("15730319191",10), bcryptjs.hashSync("Syh20001123.", 10))
 exports.regUser = (req, res) => {
     res.send('reguser ok')
 }
 
 exports.logIn = (req, res) => {
+    const userInfo = req.body
+    if(!userInfo.username || !userInfo.password) {
+        return res.send({ status: 1, message: "用户名或密码密码不合法"})
+    }
     res.send('login ok')
 }
+
