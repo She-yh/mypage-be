01f42c7279931a9ba6c16bee05153237c295085e
Sheyh <965598930@qq.com>
2022-08-29 09:51:44 +0800

    备份一下

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql')
 const db = mysql.createPool({
     host: '127.0.0.1',
-    user: 'mypage',
+    user: 'root',
     password: 'Syh20001123.',
     database: 'mypage',
     charset: 'utf8mb4'
